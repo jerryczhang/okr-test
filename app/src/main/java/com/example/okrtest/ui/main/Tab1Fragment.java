2d4959c0b924d4434c8b0cb69295b21f337c4a5c
jerryczhang <jerrychzhang@gmail.com>
2020-04-16 10:51:17 -0700

    Add java class and layout file for goal preview
    Add ItemAdapter for listing goal previews in RecyclerView, and
    goal_preview layout for each item preview.

--- app/src/main/java/com/example/okrtest/ui/main/Tab1Fragment.java
+++ app/src/main/java/com/example/okrtest/ui/main/Tab1Fragment.java
@@ -1,5 +1,7 @@
 package com.example.okrtest.ui.main;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.okrtest.ItemAdapter;
 import com.example.okrtest.R;
 
 public class Tab1Fragment extends Fragment {
@@ -20,7 +23,6 @@ public class Tab1Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -28,8 +30,15 @@ public class Tab1Fragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_tab1, container, false);
-        RecyclerView goalsRecyclerView = (RecyclerView) root.findViewById(R.id.goalsRecyclerView);
 
+        Resources res = getResources();
+        String[] goalNames = res.getStringArray(R.array.goal_names);
+
+        RecyclerView goalsRecyclerView = (RecyclerView) root.findViewById(R.id.goalsRecyclerView);
+        if (this.getContext() != null) {
+            ItemAdapter itemAdapter = new ItemAdapter(this.getContext(), goalNames);
+            goalsRecyclerView.setAdapter(itemAdapter);
+        }
         return root;
     }
 }
