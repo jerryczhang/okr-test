16670e7fdceee06f0538bce011a8897f0553f350
jerryczhang <jerrychzhang@gmail.com>
2020-04-18 10:33:52 -0700

    Launch new activity from RecyclerView
    Add new activity for displaying detail goal information. Add click
    listener for launching the activity from the RecyclerView.

---
+++ app/src/main/java/com/example/okrtest/GoalDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.okrtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoalDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_detail);
+        setTitle("Test");
+    }
+}
--- app/src/main/java/com/example/okrtest/GoalsAdapter.java
+++ app/src/main/java/com/example/okrtest/GoalsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.okrtest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,9 +19,21 @@ public class GoalsAdapter extends RecyclerView.Adapter<GoalsAdapter.ViewHolder> 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView goalNameTextView;
 
+        private final Context context;
+        public static final String EXTRA_GOAL_NAME = "com.example.okrtest.GOAL_NAME";
+
         public ViewHolder(View view) {
             super(view);
             goalNameTextView = (TextView) view.findViewById(R.id.goalNameTextView);
+
+            context = view.getContext();
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, GoalDetailActivity.class);
+                    context.startActivity(intent);
+                }
+            });
         }
 
         public void setGoalNameTextView(String text) {
