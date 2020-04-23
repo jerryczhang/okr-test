f1f64270fc8b4afce3d38777933b4a5f081a3e13
jerryczhang <jerrychzhang@gmail.com>
2020-04-23 08:45:12 -0700

    Add EditText and buttons to add goal dialog

--- app/src/main/java/com/example/okrtest/AddGoalDialogFragment.java
+++ app/src/main/java/com/example/okrtest/AddGoalDialogFragment.java
@@ -2,18 +2,35 @@ package com.example.okrtest;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
+import com.example.okrtest.ui.main.Tab1Fragment;
+
 public class AddGoalDialogFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = requireActivity().getLayoutInflater().inflate(R.layout.dialog_add_goal, null);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle(R.string.add_goal_dialog_title);
+        builder.setView(view);
+        builder.setPositiveButton(R.string.add_goal_dialog_positive, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.setNegativeButton(R.string.add_goal_dialog_negative, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                AddGoalDialogFragment.this.getDialog().cancel();
+            }
+        });
         return builder.create();
     }
 }
