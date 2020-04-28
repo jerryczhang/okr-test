90d393a7684e82f445d0ac6c45b9de92fae36428
jerryczhang <jerrychzhang@gmail.com>
2020-04-28 09:22:33 -0700

    Fix counting new items when added
    Add counter to AddGoal method to properly track the number of goals. Add
    a button to delete shared preferences data.

--- app/src/main/java/com/example/okrtest/ui/main/Tab1Fragment.java
+++ app/src/main/java/com/example/okrtest/ui/main/Tab1Fragment.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -32,6 +34,14 @@ public class Tab1Fragment extends Fragment {
     private SharedPreferences sharedPreferences;
 
     @Override
+    public void onStart() {
+        super.onStart();
+        loadGoals();
+        //clearSharedPreferences();
+        Log.println(Log.ASSERT, "goals_array", goalNames.toString());
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
@@ -43,7 +53,6 @@ public class Tab1Fragment extends Fragment {
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_tab1, container, false);
 
-        loadGoals();
         RecyclerView goalsRecyclerView = (RecyclerView) root.findViewById(R.id.goalsRecyclerView);
 
         goalsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -61,13 +70,22 @@ public class Tab1Fragment extends Fragment {
             }
         });
 
+        Button clearSharedPrefButton = (Button) root.findViewById(R.id.clearSharedPrefButton);
+        clearSharedPrefButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearSharedPreferences();
+            }
+        });
         return root;
     }
 
     public void addGoal(String name) {
         goalNames.add(name);
         goalsAdapter.notifyItemInserted(goalNames.size() - 1);
+        numGoals += 1;
         saveGoals();
+        Log.println(Log.ASSERT, "goals_array", goalNames.toString());
     }
 
     private void saveGoals() {
@@ -83,10 +101,17 @@ public class Tab1Fragment extends Fragment {
         int defaultNumGoals = 0;
         String defaultGoalName = "";
         numGoals = sharedPreferences.getInt(getString(R.string.num_goals), defaultNumGoals);
+        goalNames.clear();
         for (int i = 0; i < numGoals; ++i) {
             String goalName = sharedPreferences.getString(getString(R.string.goal) + i, defaultGoalName);
             goalNames.add(goalName);
             goalsAdapter.notifyItemInserted(goalNames.size() - 1);
         }
+    }
+
+    private void clearSharedPreferences() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
     }
 }
