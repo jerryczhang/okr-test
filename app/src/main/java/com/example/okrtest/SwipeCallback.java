81bdc7e1aad51ee7f3cf88309966702bc6d351e3
jerryczhang <jerrychzhang@gmail.com>
2020-07-08 10:30:25 -0700

    Show color behind round edges on kr preview swipe

--- app/src/main/java/com/example/okrtest/SwipeCallback.java
+++ app/src/main/java/com/example/okrtest/SwipeCallback.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.view.View;
 import android.widget.Adapter;
 
@@ -18,16 +19,18 @@ public class SwipeCallback extends ItemTouchHelper.SimpleCallback {
     private RecyclerView.Adapter adapter;
     private Drawable leftIcon;
     private Drawable rightIcon;
-    private ColorDrawable leftBackground;
-    private ColorDrawable rightBackground;
+    private GradientDrawable leftBackground;
+    private GradientDrawable rightBackground;
 
     public SwipeCallback(RecyclerView.Adapter adapter, Context context) {
         super(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT);
         this.adapter = adapter;
         leftIcon = ContextCompat.getDrawable(context, R.drawable.baseline_create_black_48dp);
         rightIcon = ContextCompat.getDrawable(context, R.drawable.baseline_delete_black_48dp);
-        leftBackground = new ColorDrawable(ContextCompat.getColor(context, R.color.TOMATO));
-        rightBackground = new ColorDrawable(ContextCompat.getColor(context, R.color.KHAKI));
+        leftBackground = new GradientDrawable();
+        rightBackground = new GradientDrawable();
+        leftBackground.setColor(ContextCompat.getColor(context, R.color.TOMATO));
+        rightBackground.setColor(ContextCompat.getColor(context, R.color.KHAKI));
     }
 
     @Override
@@ -44,11 +47,11 @@ public class SwipeCallback extends ItemTouchHelper.SimpleCallback {
     public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
 
-        View itemView = viewHolder.itemView;
         int backgroundCornerOffset = 20;
+        View itemView = viewHolder.itemView;
         if (dX > 0) {
             leftBackground.setBounds(itemView.getLeft(), itemView.getTop(), itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
-            //rightBackground.setBounds(0, 0, 0, 0);
+            rightBackground.setBounds(0, 0, 0, 0);
         } else if (dX < 0) {
             rightBackground.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset, itemView.getTop(), itemView.getRight(), itemView.getBottom());
             leftBackground.setBounds(0, 0, 0, 0);
