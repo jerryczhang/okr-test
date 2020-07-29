4db483f89b018f53eda1fd748948a0643823d8d4
jerryczhang <jerrychzhang@gmail.com>
2020-07-29 13:51:02 -0700

    Remove action dots, rename gallery to friends
    Delete action dots for settings, rename gallery and usages to friends.

---
+++ app/src/main/java/com/example/okrtest/ui/friends/FriendsFragment.java
@@ -0,0 +1,22 @@
+package com.example.okrtest.ui.friends;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.okrtest.R;
+
+public class FriendsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_friends, container, false);
+        final TextView textView = root.findViewById(R.id.text_gallery);
+        return root;
+    }
+}
