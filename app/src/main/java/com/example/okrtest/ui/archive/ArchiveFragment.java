0386fab56367fd4aed4ae3cab7fe895580c1ffc2
jerryczhang <jerrychzhang@gmail.com>
2020-08-04 10:27:31 -0700

    Change slideshow fragment to archive

---
+++ app/src/main/java/com/example/okrtest/ui/archive/ArchiveFragment.java
@@ -0,0 +1,20 @@
+package com.example.okrtest.ui.archive;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.okrtest.R;
+
+public class ArchiveFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_archive, container, false);
+        return root;
+    }
+}
