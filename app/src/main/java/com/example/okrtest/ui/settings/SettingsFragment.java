1743c08a495239e12706be3dac4f3f811370e443
jerryczhang <jerrychzhang@gmail.com>
2020-08-10 11:27:16 -0700

    Populate settings tab with default options

--- app/src/main/java/com/example/okrtest/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/okrtest/ui/settings/SettingsFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.okrtest.R;
 
@@ -15,6 +16,19 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_settings, container, false);
+        super.onCreate(savedInstanceState);
+        getParentFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment.PrefFragment())
+                .commit();
         return root;
     }
+
+    public static class PrefFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
 }
+
